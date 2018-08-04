b137b10322e820126e7bbea673c0a52dbd6bcb41
Rus Marcus Florin <rus.marcus@yahoo.com>
2018-08-04 09:29:12 +0300

    something changend

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,9 @@ carousel();
         x[i].style.display = "none";  
         }
         myIndex++;
-        if (myIndex > x.length) {myIndex = 1}    
+        if (myIndex > x.length) {
+            myIndex = 1
+        }    
         x[myIndex-1].style.display = "block";  
         setTimeout(carousel, 3000); // Change image every 3 seconds
     }
